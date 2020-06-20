ea9bf5325ac2cac3aa6b03d2e1438d1a344ad25f
ozaki <clement.couriol3+desktop@gmail.com>
2020-06-20 18:46:33 +0200

    script fix overwriting electron js file

--- script/copybundle.cmd
+++ script/copybundle.cmd
@@ -1,4 +1,4 @@
-copy /V /Y build\react\bundle\bundle_main.js src\electron\main.js
+REM copy /V /Y build\react\bundle\bundle_main.js src\electron\main.js
 copy /V /Y build\react\bundle\bundle_react_scss_ui.js src\frontend\public\bundle_react_scss_ui.js
 copy /V /Y build\react\bundle\bundle_react_scss_ui.js src\frontend\public\public_scss_ui\bundle_react_scss_ui.js
 copy /V /Y build\react\bundle\bundle_react_material_ui.js src\frontend\public\bundle_react_material_ui.js
--- script/copybundle.sh
+++ script/copybundle.sh
@@ -1,4 +1,4 @@
-cp -f ./build/react/bundle/bundle_main.js ./src/electron/main.js
+# cp -f ./build/react/bundle/bundle_main.js ./src/electron/main.js
 cp -f ./build/react/bundle/bundle_react_scss_ui.js ./src/frontend/public/bundle_react_scss_ui.js
 cp -f ./build/react/bundle/bundle_react_scss_ui.js ./src/frontend/public/public_scss_ui/bundle_react_scss_ui.js
 cp -f ./build/react/bundle/bundle_react_material_ui.js ./src/frontend/public/bundle_react_material_ui.js
