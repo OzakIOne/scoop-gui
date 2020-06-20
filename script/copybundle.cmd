b1708bad9f3cbddae0b19d47b04b1c0136bf744f
ozaki <clement.couriol3+desktop@gmail.com>
2020-06-20 18:31:51 +0200

    script update

---
+++ script/copybundle.cmd
@@ -0,0 +1,5 @@
+copy /V /Y build\react\bundle\bundle_main.js src\electron\main.js
+copy /V /Y build\react\bundle\bundle_react_scss_ui.js src\frontend\public\bundle_react_scss_ui.js
+copy /V /Y build\react\bundle\bundle_react_scss_ui.js src\frontend\public\public_scss_ui\bundle_react_scss_ui.js
+copy /V /Y build\react\bundle\bundle_react_material_ui.js src\frontend\public\bundle_react_material_ui.js
+copy /V /Y build\react\bundle\bundle_react_material_ui.js src\frontend\public\public_material_ui\bundle_react_material_ui.js
